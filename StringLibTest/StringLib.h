dfc52790f8b785c25de69f1a8d28eda293e76e68
Daehder <erikm95@me.com>
2016-04-06 17:38:01 -0700

    Added StringLibTest
    Added StringLib, which splits input strings into two 16 character lines
    and an extra line

---
+++ StringLibTest/StringLib.h
@@ -0,0 +1,14 @@
+//
+//  StringLib.h
+//  StringLibTest
+//
+//  Created by Erik Miller on 4/6/16.
+//  Copyright © 2016 Daehder. All rights reserved.
+//
+
+#ifndef StringLib_h
+void getLines(char *input, char **line1, char **line2, char **extra);
+#define StringLib_h
+
+
+#endif /* StringLib_h */
