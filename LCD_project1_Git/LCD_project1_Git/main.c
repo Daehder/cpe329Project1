768d3752a0f541f2b2b09475081dc0284dec1c22
mytchj <mytchj@gmail.com>
2016-04-07 19:34:53 -0700

    Cleaned up code and made buttons run display
    Your fancy print function no longer works

--- LCD_project1_Git/LCD_project1_Git/main.c
+++ LCD_project1_Git/LCD_project1_Git/main.c
@@ -5,18 +5,17 @@
  * Author : Mytch Johnson
  */ 
 
-#define F_CPU 16000000UL
-
-#define func_set 0x38
-#define disp_ctrl 0x0F
-#define disp_clear 0x01
-#define entry_mode 0x06
-#define set_line2 0xC0
-#define set_line1 0x02
-#define shift_right 0x05
-#define shift_left 0x07
-#define inc_right 0x06
-#define inc_left 0x04
+#define F_CPU 16000000UL	// set CPU speed to 16MHz
+#define std_delay 50		// set standard delay time to 50us
+#define debounce 100		// set debounce time to 50ms
+#define func_set 0x38		// 8bit command for function set
+#define disp_ctrl 0x0F		// 8bit command for display control
+#define disp_clear 0x01		// 8bit command for display clear
+#define entry_mode 0x06		// 8bit command for enable entry
+#define set_line1 0x02		// address for start of line 2
+#define set_line2 0xC0		// address for start of line 1
+#define right_button 0x01	// return type for right button
+#define left_button 0x02	// return type for left button
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -24,10 +23,13 @@
 #include <stdlib.h>
 #include "StringLibTest/StringLib.h"
 
+// function prototypes 
 void lcd_write_cmd(char command);
 void lcd_write_char(char character);
 void lcd_initialize();
 void lcd_print_string(char string[]);
+void clear_display(); 
+uint8_t check_buttons(); 
 
 int main(void)
 {
@@ -41,12 +43,15 @@ int main(void)
 	lcd_initialize();
 	
 	// print to the LCD
-	lcd_print_string("Hello World! Iamveryhappytotrythisout!");
+	lcd_print_string("Hello World!... Goodbye World!");
 	
 	while(1){
-		//if button is pressed
-		if((~PINB & 0b00010000) | (~PINB & 0b00001000))
+		// if a button is pressed
+		if(check_buttons()){
 			PORTB |= 0b00100000;	// turn on led
+			clear_display();
+			lcd_print_string("I like big butts and I can not lie!");
+		}
 		else
 			PORTB &= 0b11011111;	// turn off led
 	}
@@ -57,58 +62,84 @@ int main(void)
 // runs pins and delays to send LCD a command 
 void lcd_write_cmd(char command){
 	PORTB &= 0b11111000;    // E, RW, RS = 0,0,0
-	_delay_us(1);			// delay
 	PORTD = command;		// set command
-	_delay_us(1);			// delay
 	PORTB |= 0b00000100;	// E, RW, RS = 1,0,0
-	_delay_us(1);			// delay 
 	PORTB &= 0b11111000;	// E, RW, RS = 0,0,0
-	_delay_us(50);
+	_delay_us(std_delay);
+}
+
+// runs LCD initialization code
+void lcd_initialize(){
+	_delay_ms(35);					// power ON delay > 30ms
+	lcd_write_cmd(func_set);		// run function set
+	_delay_us(std_delay);			// delay > 50us
+	lcd_write_cmd(disp_ctrl);		// run display set (off)
+	_delay_us(std_delay);			// delay > 50us
+	lcd_write_cmd(disp_clear);		// run clear display
+	_delay_ms(2);					// delay > 1.5 ms
+	lcd_write_cmd(entry_mode);		// set entry mode
+	_delay_us(std_delay);			// delay > 50us
 }
 
 //  runs pins and delays to have LCD print characters 
 void lcd_write_char(char character){
 	PORTB &= 0b11111000;    // E, RW, RS = 0,0,0
-	_delay_us(1);			// delay
 	PORTD = character;		// send character
-	_delay_us(1);			// delay
 	PORTB |= 0b00000101;	// E, RW, RS = 1,0,1
-	_delay_us(1);			// delay
 	PORTB &= 0b11111000;	// E, RW, RS = 0,0,0
-	_delay_us(50);
+	_delay_us(std_delay);			// delay
 }
 
-// runs LCD initialization code
-void lcd_initialize(){
-	_delay_ms(50);			// power ON delay > 30ms
-	
-	// run setup
-	lcd_write_cmd(func_set);		// run function set
-	_delay_us(80);					// delay > 4.5ms
-	lcd_write_cmd(disp_ctrl);		// run display set (off)
-	_delay_us(80);					// delay > 50us
-	lcd_write_cmd(disp_clear);		// run clear display
-	_delay_ms(3);					// delay > 3ms
-	lcd_write_cmd(entry_mode);		// set entry mode
-	_delay_us(80);					// delay > 50us
+// check which (if any) buttons are pressed
+uint8_t check_buttons(){
+	// if pin12 (left) button is pressed
+	if(~PINB & 0b00010000){
+		_delay_ms(debounce);
+		return left_button;
+	}
+	// if pin11 (right) button is pressed
+	else if(~PINB & 0b00001000){
+		_delay_ms(debounce);
+		return right_button;
+	}
+	// if no button is pressed
+	else
+		return 0;
 }
 
 // prints string of characters to LCD
 void lcd_print_string(char string[]){
-	char *line1, *line2, *extra;
+for(int x=0; x<strlen(string); x++){
+	if(x==16){
+		lcd_write_cmd(set_line2);
+		_delay_us(std_delay);
+	}
+	lcd_write_char(string[x]);
+	_delay_us(std_delay);
+}
 	
+/*
+	char *line1, *line2, *extra;
 	getLines(string, &line1, &line2, &extra);
 	
-	for(int j=0; j<strlen(line1); j++){
-		lcd_write_char(line1[j]);
-		_delay_us(100);
+	// print top line
+	for(int i=0; i<strlen(line1); i++){
+		lcd_write_char(line1[i]);
+		_delay_us(std_delay);
 	}
 	
+	// set cursor to beginning of line 2
 	lcd_write_cmd(set_line2);
 	
-	for(int i=0; i<strlen(line2); i++){
-		
-		lcd_write_char(line2[i]);
-		_delay_us(100);
+	// print bottom line 
+	for(int j=0; j<strlen(line2); j++){
+		lcd_write_char(line2[j]);
+		_delay_us(std_delay);
 	}
+*/
+}
+
+void clear_display(){
+	lcd_write_cmd(disp_clear);
+	_delay_ms(2);
 }
